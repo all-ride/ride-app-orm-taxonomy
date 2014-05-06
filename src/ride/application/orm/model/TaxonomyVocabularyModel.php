80c344adbbecbe9cc058fb1f0ea5d81025f1636d
Joris Vandeweerd <joris@statik.be>
2014-05-06 16:22:04 +0200

    initial commit

---
+++ src/ride/application/orm/model/TaxonomyVocabularyModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ride\application\orm\model;
+
+use ride\library\orm\model\GenericModel;
+
+/**
+ * Model for the taxonomy vocabularies
+ */
+class TaxonomyVocabularyModel extends GenericModel {
+
+}
